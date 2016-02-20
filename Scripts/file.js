8399c2b55187ff04809c0938941ecbf9e6c1adad
michal_urbanski <michal_urbanski@windowslive.com>
2016-02-20 20:03:52 +0100

    Adds error handling logic

--- Scripts/file.js
+++ Scripts/file.js
@@ -15,12 +15,27 @@ var fs = require('fs');
 // In node this has to be done like below
 fs.open('sample.txt', 'r',
   function(err, handle){
+    // errors handling logic 
+    if(err){
+        handleError(err); 
+        return; 
+    }
+    
     var buf = new Buffer(10000);
 
     fs.read(handle, buf, 0, 10000, null,
       function(err, length){
+          if(err){
+              handleError(err); 
+              return; 
+          }
+          
         console.log("Printing file:");
         console.log(buf.toString('utf8', 0, length));
         fs.close(handle, function(){/* don't care */})
       });
 });
+
+function handleError(err){
+    console.log("Error: " + err.code + " ErrorMessage: " + err.message);
+}
